190fe6636fd159ec758544ad6acb5d4698c03fb9
Yonnick Brown <25hour24@gmail.com>
2019-01-11 15:59:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,12 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, position)
-  if board[position] == "" || " "
-    false
-  elsif board[position] == "X" || "O"
-    true
-  end
-end
+# def position_taken?(board, position)
+#   if board[position] == "" || " "
+#     false
+#   elsif board[position] == "X" || "O"
+#     true
+#   end
+# end
 
 
 def position_taken?(board, index)
